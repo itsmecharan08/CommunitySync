f3f0cbe5aca4bece3db2d0b1d512b4170209364c
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2024-03-26 11:17:25 +0530

    Connected to DB

--- server-inator/server.js
+++ server-inator/server.js
@@ -8,6 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGODB_LOCAL_URL, {})
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("Error occured" + err);
+  });
+
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
