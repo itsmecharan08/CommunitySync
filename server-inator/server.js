c5ecc4700557b417824c6b0f1acf632faa51c771
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2024-04-23 21:29:17 +0530

    Added JSON Web Token Login System

--- server-inator/server.js
+++ server-inator/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const mongoose = require("mongoose");
 const customer = require("./models/customer");
@@ -13,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 mongoose
   .connect(process.env.MONGODB_LOCAL_URL, {})
@@ -76,6 +79,46 @@ app.post("user/auth/register", async (req, res) => {
       .json({ status: "Error", error: "Internal Server Error" });
   }
 });
+
+app.post("/auth/login", (req, res) => {
+  let user;
+  user = customer.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!user) {
+    user = supplier.find(
+      (u) => u.username === username && u.password === password
+    );
+  }
+  if (!user) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+  const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+    expiresIn: "1h",
+  });
+  console.log(token);
+  res.json({ token });
+});
+
+app.get("/protected", verifyToken, (req, res) => {
+  res.json({ message: "Protected route accessed successfully" });
+});
+
+function verifyToken(req, res, next) {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(403).json({ message: "Token is required" });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+}
 
 // app.get("/demo", (req, res) => {
 //   console.log("Get requested received");
