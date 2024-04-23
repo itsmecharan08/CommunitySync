29164247cafbaf849052ba1f34190e1d04a7cef1
I-SHANKAR-NARAYANA <shankara.vadhulasa@gmail.com>
2024-04-23 19:59:17 +0530

    Added API for customer auth

--- server-inator/server.js
+++ server-inator/server.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
+const bcrypt = require("bcryptjs");
+
 const mongoose = require("mongoose");
+const customer = require("./models/customer");
+const supplier = require("./models/supplier");
+const service = require("./models/service");
 
 const app = express();
 
@@ -17,14 +23,37 @@ mongoose
     console.log("Error occured" + err);
   });
 
-app.post("/client", (req, res) => {
-  res.send(req.body);
+app.post("/client/auth", async (req, res) => {
+  try {
+    const found = await customer.findOne({ email: req.body.email });
+    if (found) {
+      return res.json({ status: "Error", error: "Duplicate Email" });
+    } else {
+      const customerData = {};
+      for (const key in req.body) {
+        if (req.body.hasOwnProperty(key) && req.body[key]) {
+          customerData[key] = req.body[key];
+        }
+      }
+      customerData[password] = await bcrypt.hash(req.body.password, 10);
+      await customer.create(customerData);
+      return res.json({
+        status: "Success",
+        message: "Customer created successfully",
+      });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .json({ status: "Error", error: "Internal Server Error" });
+  }
 });
 
-app.get("/demo", (req, res) => {
-  console.log("Get requested received");
-  res.send("Demo");
-});
+// app.get("/demo", (req, res) => {
+//   console.log("Get requested received");
+//   res.send("Demo");
+// });
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
